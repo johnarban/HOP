18473e4c0d84e921e5800ab107906848591de362
johnarban <jaffagate1@gmail.com>
2021-10-16 15:08:44 -0400

    formatting

--- plot_and_reduce.py
+++ plot_and_reduce.py
@@ -15,85 +15,80 @@ hg = np.loadtxt('hgar_blue.txt') / 10
 ar = np.loadtxt('argon_red.txt') / 10
 
 
-
-def plot_reduce(cal_file, data_file, cal_threshold=0.05, bottom = None, top = None, rectify=True, order=2,
-           plot=True,recal='n',cosmic_rays=False,save=False, slice_fwhm = 1.5,size=5):
+def plot_reduce(cal_file, data_file, cal_threshold=0.05, bottom=None, top=None, rectify=True, order=2,
+                plot=True, recal='n', cosmic_rays=False, save=False, slice_fwhm=1.5, size=5):
     cal = fits.getdata(cal_file).astype(float)
     cal = cal + float(hf.getheader_val(cal_file, 'PEDESTAL'))
 
-
     data = fits.getdata(data_file).astype(float)
-    obj = hf.getheader_val(data_file,'OBJECT')
+    obj = hf.getheader_val(data_file, 'OBJECT')
     data = data + float(hf.getheader_val(data_file, 'PEDESTAL'))
 
-    out = hf.get_wavelength_cal(cal_file, order = order,
+    out = hf.get_wavelength_cal(cal_file, order=order,
                                 threshold=cal_threshold,
-                                size=size,recal=recal)
+                                size=size, recal=recal)
     p, wavesol, cal_spec, rect_sol = out
 
     with plt.rc_context(rc={'image.origin': 'upper'}):
         if plot:
             mosaic = """AABB;CCDD;.II.;EEFF;GGHH"""
-            fig, axs = plt.subplot_mosaic(mosaic,gridspec_kw={'height_ratios':[1,1,.7,1,1.25]},figsize=(12,16))
+            fig, axs = plt.subplot_mosaic(mosaic, gridspec_kw={'height_ratios': [1, 1, .7, 1, 1.25]}, figsize=(12, 16))
         else:
             mosaic = """GH"""
-            fig, axs = plt.subplot_mosaic(mosaic,figsize=(10,4))
-            axs['A']=None
-            axs['B']=None
-            axs['C']=None
-            axs['D']=None
-            axs['E']=None
-            axs['F']=None
-            axs['I']=None
+            fig, axs = plt.subplot_mosaic(mosaic, figsize=(10, 4))
+            axs['A'] = None
+            axs['B'] = None
+            axs['C'] = None
+            axs['D'] = None
+            axs['E'] = None
+            axs['F'] = None
+            axs['I'] = None
 
         # rect_sol, full_frame_solution = rectify_ccd(cal,plot=plot,ax=axs['A']);
 
-
         if rectify:
-            cal = hf.shift_row_interp(cal,rect_sol,plot=plot,axs=(axs['A'],axs['B']))
-            data = hf.shift_row_interp(data,rect_sol,plot=plot,axs=(axs['C'],axs['D']))
+            cal = hf.shift_row_interp(cal, rect_sol, plot=plot, axs=(axs['A'], axs['B']))
+            data = hf.shift_row_interp(data, rect_sol, plot=plot, axs=(axs['C'], axs['D']))
         else:
-            hf.shift_row_interp(cal,rect_sol,plot=plot,axs=(axs['A'],None))
-            hf.shift_row_interp(data,rect_sol,plot=plot,axs=(axs['C'],None))
+            hf.shift_row_interp(cal, rect_sol, plot=plot, axs=(axs['A'], None))
+            hf.shift_row_interp(data, rect_sol, plot=plot, axs=(axs['C'], None))
 
-
-
-        sl = hf.specextract(data,bottom=bottom,top=top,plot=plot,ax=axs['I'],slice_fwhm=slice_fwhm)
+        sl = hf.specextract(data, bottom=bottom, top=top, plot=plot, ax=axs['I'], slice_fwhm=slice_fwhm)
 
         # plot the section we will extract
         if plot:
-            vmin, vmax  = np.percentile(cal,[50,99])
-            norm = mc.SymLogNorm(100,vmin=vmin,vmax=vmax)
+            vmin, vmax = np.percentile(cal, [50, 99])
+            norm = mc.SymLogNorm(100, vmin=vmin, vmax=vmax)
             ax = axs['E']
-            ax.imshow(cal,norm=norm,cmap='viridis')
-            ax.axhline(sl.start,color='r')
-            ax.axhline(sl.stop,color='r')
+            ax.imshow(cal, norm=norm, cmap='viridis')
+            ax.axhline(sl.start, color='r')
+            ax.axhline(sl.stop, color='r')
             ax.set_title('Calibration')
             ax = axs['F']
-            vmin, vmax  = np.percentile(data,[50,99])
-            norm = mc.SymLogNorm(100,vmin=vmin,vmax=vmax)
-            ax.imshow(data,norm=norm,cmap='viridis')
-            ax.axhline(sl.start,color='r')
-            ax.axhline(sl.stop,color='r')
+            vmin, vmax = np.percentile(data, [50, 99])
+            norm = mc.SymLogNorm(100, vmin=vmin, vmax=vmax)
+            ax.imshow(data, norm=norm, cmap='viridis')
+            ax.axhline(sl.start, color='r')
+            ax.axhline(sl.stop, color='r')
             ax.set_title('Target')
 
         # Get wavelength calibration
-        cal_spec = np.nanmean(cal,axis=0)
-        back = hf.findback1d(cal_spec,s=20)
-        peaks = hf.find_peaks(cal_spec - back,threshold=cal_threshold,size=size,) # put in wavelength order
+        cal_spec = np.nanmean(cal, axis=0)
+        back = hf.findback1d(cal_spec, s=20)
+        peaks = hf.find_peaks(cal_spec - back, threshold=cal_threshold, size=size,)  # put in wavelength order
         peaks, peaks_y = peaks.T
-        linelist = np.append(hg,ar)
-        new = np.polyval(p,peaks)
-        matched = linelist[np.argmin(np.abs(new - linelist[:,np.newaxis]),axis=0)]
+        linelist = np.append(hg, ar)
+        new = np.polyval(p, peaks)
+        matched = linelist[np.argmin(np.abs(new - linelist[:, np.newaxis]), axis=0)]
 
         ax = axs['G']
-        #Plot wavelength cal
-        ax.plot(wavesol,cal_spec,lw=1)
+        # Plot wavelength cal
+        ax.plot(wavesol, cal_spec, lw=1)
         x = np.arange(len(cal_spec))
-        #ax.plot(np.polyval(p,peaks),peaks_y,'r.')
-        ax.plot(wavesol,back,lw=1)
-        for i in np.polyval(p,peaks):
-            ax.axvline(i,color='0.5',zorder=0,lw=.5)
+        # ax.plot(np.polyval(p,peaks),peaks_y,'r.')
+        ax.plot(wavesol, back, lw=1)
+        for i in np.polyval(p, peaks):
+            ax.axvline(i, color='0.5', zorder=0, lw=.5)
         ax.invert_xaxis()
         ax.set_title('wavelength cal')
         ax.set_xlabel('Wavelength [nm]')
@@ -105,26 +100,25 @@ def plot_reduce(cal_file, data_file, cal_threshold=0.05, bottom = None, top = No
             im2[:, :, 3] = cr * 1
             if axs['F'] is not None:
                 axs['F'].imshow(im2)
-            spec = np.ma.mean(np.ma.masked_array(data,cr)[sl,:],axis=0)
+            spec = np.ma.mean(np.ma.masked_array(data, cr)[sl, :], axis=0)
         else:
-            spec = np.mean(data[sl,:],axis=0)
-
+            spec = np.mean(data[sl, :], axis=0)
 
         #noise = np.nanstd(data[sl,:],axis=0)/np.sqrt(top-bot)
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            noise = astats.sigma_clipped_stats(data,sigma=3,)[-1] / np.sqrt(sl.stop-sl.start)
+            noise = astats.sigma_clipped_stats(data, sigma=3,)[-1] / np.sqrt(sl.stop-sl.start)
             noise = np.sqrt(spec + noise**2)
 
         ax = axs['H']
-        hf.errorbar_fill(wavesol,spec,noise,mid=True,alpha=1,lw=.5,ax=ax)
-        #plt.plot(wavesol,data[sl,:].T,'k',lw=.5)
+        hf.errorbar_fill(wavesol, spec, noise, mid=True, alpha=1, lw=.5, ax=ax)
+        # plt.plot(wavesol,data[sl,:].T,'k',lw=.5)
 
         ax.set_xlabel('Wavelength [nm]')
         ax.set_ylabel('Uncalibrated data units')
         ax.set_title(f'Spectrum of {obj}')
 
-        out = np.array(list(zip(wavesol,spec,noise,cal_spec-back)))
+        out = np.array(list(zip(wavesol, spec, noise, cal_spec-back)))
         fname = f"{data_file.replace('.FIT','.tsv')}"
 
         if save:
@@ -133,41 +127,41 @@ def plot_reduce(cal_file, data_file, cal_threshold=0.05, bottom = None, top = No
 #                 f.write('wavelength\tspectrum\terror\tcal\n')
 #                 for i in out:
 #                     f.write('{:<9.3f}\t{:>10.3f}\t{:>10.3f}\t{:>10.3f}\n'.format(*i))
-            np.savetxt(fname,out,fmt=('%-9.3f\t%-10.3f\t%-10.3f\t%-10.3f'),header='wave spec err cal')
+            np.savetxt(fname, out, fmt=('%-9.3f\t%-10.3f\t%-10.3f\t%-10.3f'), header='wave spec err cal')
 
-        return (wavesol,spec,noise),fig
-
-
-
+        return (wavesol, spec, noise), fig
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Reduce a spectrum from the spectrograph on the Clay Telescope (Harvard University)")
+    parser = argparse.ArgumentParser(
+        description="Reduce a spectrum from the spectrograph on the Clay Telescope (Harvard University)")
 
-    parser.add_argument("-c","--cal",help="""The calibration file""")
+    parser.add_argument("-c", "--cal", help="""The calibration file""")
 
-    parser.add_argument("-d","--data",help="The spectrum we want reduced")
+    parser.add_argument("-d", "--data", help="The spectrum we want reduced")
 
-    parser.add_argument("-b","--bottom",default = None,type=int,help="lower index for spectrum boundary")
+    parser.add_argument("-b", "--bottom", default=None, type=int, help="lower index for spectrum boundary")
 
-    parser.add_argument("-t","--top",default = None,type=int,help="higher index for spectrum boundary")
+    parser.add_argument("-t", "--top", default=None, type=int, help="higher index for spectrum boundary")
 
-    parser.add_argument("--threshold",default = 0.05,type=float,help="Threshold level used for defining getting peaks for calibration")
+    parser.add_argument("--threshold", default=0.05, type=float,
+                        help="Threshold level used for defining getting peaks for calibration")
 
-    parser.add_argument("--width",default = 1.5,type=float,help="Number of FWHMs to use for extracting spectrum")
+    parser.add_argument("--width", default=1.5, type=float, help="Number of FWHMs to use for extracting spectrum")
 
-    parser.add_argument("--plot",action='store_true',help="Plot only the spectrum")
+    parser.add_argument("--plot", action='store_true', help="Plot only the spectrum")
 
-    parser.add_argument("-r","--recalibrate",action='store_true',help="Force recalibration. Otherwise it will silently use a previous calibration if one exists")
+    parser.add_argument("-r", "--recalibrate", action='store_true',
+                        help="Force recalibration. Otherwise it will silently use a previous calibration if one exists")
 
     # parser.add_argument("-s","--dont-save",action='store_false',help="Don't Rectify the spectra?")
 
-    parser.add_argument("--batch",action='store_true',help="Don't open images")
+    parser.add_argument("--batch", action='store_true', help="Don't open images")
 
-    parser.add_argument("-o","--order",default=2,type=int,help="Don't open images")
+    parser.add_argument("-o", "--order", default=2, type=int, help="Don't open images")
 
-    parser.add_argument("--zap-cosmic-rays",default=False,help=""""Get rid of cosmic rays.
+    parser.add_argument("--zap-cosmic-rays", default=False, help=""""Get rid of cosmic rays.
                                                         This uses simple algorithm that could clip bright lines.
                                                         Best used only for long integrations of diffuse nebular emission""")
 
@@ -181,8 +175,8 @@ if __name__ == "__main__":
     bottom = args.bottom
     top = args.top
 
-    rectify = True #args.dont_rectify
-    save = True #args.dont_save
+    rectify = True  # args.dont_rectify
+    save = True  # args.dont_save
 
     zap_cosmic_rays = args.zap_cosmic_rays
 
@@ -200,8 +194,8 @@ if __name__ == "__main__":
     else:
         recal = 'n'
 
-    out,fig = plot_reduce(cal_file, data_file, cal_threshold=threshold, bottom = bottom, top = top, order=order, rectify=rectify, cosmic_rays=zap_cosmic_rays, plot=plot, save=save, slice_fwhm=slice_fwhm, recal=recal)
-
+    out, fig = plot_reduce(cal_file, data_file, cal_threshold=threshold, bottom=bottom, top=top, order=order,
+                           rectify=rectify, cosmic_rays=zap_cosmic_rays, plot=plot, save=save, slice_fwhm=slice_fwhm, recal=recal)
 
     fname = f"{data_file.replace('.FIT','_big.png')}"
     fig.savefig(fname)
--- reduce.py
+++ reduce.py
@@ -12,8 +12,8 @@ import helper_funcs as hf
 import wavelength_cal as wc
 
 
-def reduce(cal_file, data_file, cal_threshold=0.05, bottom = None, top = None, rectify=True,
-           plot=False,clip_cal=False,cosmic_rays=False,save=True,order=2,size=5,slice_fwhm=1.5,
+def reduce(cal_file, data_file, cal_threshold=0.05, bottom=None, top=None, rectify=True,
+           plot=False, clip_cal=False, cosmic_rays=False, save=True, order=2, size=5, slice_fwhm=1.5,
            recal='n'):
     """
     reduce(cal_file, data_file, bottom = #, top = #, save = True, order = 2)
@@ -22,88 +22,85 @@ def reduce(cal_file, data_file, cal_threshold=0.05, bottom = None, top = None, r
     cal = fits.getdata(cal_file).astype(float)
     cal = cal + float(hf.getheader_val(cal_file, 'PEDESTAL'))
 
-
     data = fits.getdata(data_file).astype(float)
-    obj = hf.getheader_val(data_file,'OBJECT')
+    obj = hf.getheader_val(data_file, 'OBJECT')
     data = data + float(hf.getheader_val(data_file, 'PEDESTAL'))
 
     # 1) Rectify data & Get wavelength calibration
-    out = hf.get_wavelength_cal(cal_file, order = order,
+    out = hf.get_wavelength_cal(cal_file, order=order,
                                 threshold=cal_threshold,
-                                size=size,recal=recal)
+                                size=size, recal=recal)
     p, wavesol, cal_spec, rect_sol = out
 
     # y, x_line  = np.indices(cal.shape)
     # full_frame_solution = x_line - np.polyval(p_disp,y)
     # rect_sol, full_frame_solution = rectify_ccd(cal);
     if rectify:
-        cal = hf.shift_row_interp(cal,rect_sol)
-        data = hf.shift_row_interp(data,rect_sol)
+        cal = hf.shift_row_interp(cal, rect_sol)
+        data = hf.shift_row_interp(data, rect_sol)
 
     # remove offset/bias
 
-
     # 2) Extract selection for spectral regions
-    sl = hf.specextract(data,bottom=bottom,top=top,slice_fwhm=slice_fwhm)
+    sl = hf.specextract(data, bottom=bottom, top=top, slice_fwhm=slice_fwhm)
 
     # 3) Get wavelength calibration
     # p, wavesol, cal_spec = get_wavelength_cal(cal[sl,:],order=order)
 
-
     # 4) Some targets are cosmic rays (this is very simplistic)
     if cosmic_rays:
         cr = hf.get_cosmic_rays(data)
-        spec = np.ma.mean(np.ma.masked_array(data,cr)[sl,:],axis=0)
+        spec = np.ma.mean(np.ma.masked_array(data, cr)[sl, :], axis=0)
     else:
-        spec = np.mean(data[sl,:],axis=0)
-
+        spec = np.mean(data[sl, :], axis=0)
 
     # 5) Measure the noise
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        noise = astats.sigma_clipped_stats(data,sigma=3,)[-1] / np.sqrt(sl.stop-sl.start)
+        noise = astats.sigma_clipped_stats(data, sigma=3,)[-1] / np.sqrt(sl.stop-sl.start)
         noise = np.sqrt(spec + noise**2)
 
-    out = list(zip(wavesol,spec,noise))
+    out = list(zip(wavesol, spec, noise))
     if save:
         fname = f"{data_file.replace('.FIT','.tsv')}"
-        with open(fname,'w') as f:
+        with open(fname, 'w') as f:
             f.write('wavelength\tspectrum\terror\n')
             for i in out:
                 f.write('{:<9.3f}\t{:>10.3f}\t{:>10.3f}\n'.format(*i))
 
-    return wavesol,spec,noise
-
-
+    return wavesol, spec, noise
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Reduce a spectrum from the spectrograph on the Clay Telescope (Harvard University)")
+    parser = argparse.ArgumentParser(
+        description="Reduce a spectrum from the spectrograph on the Clay Telescope (Harvard University)")
 
-    parser.add_argument("-c","--cal",help="""The calibration file""")
+    parser.add_argument("-c", "--cal", help="""The calibration file""")
 
-    parser.add_argument("-d","--data",help="The spectrum we want reduced")
+    parser.add_argument("-d", "--data", help="The spectrum we want reduced")
 
-    parser.add_argument("-b","--bottom",default = None,type=int,help="lower index for spectrum boundary")
+    parser.add_argument("-b", "--bottom", default=None, type=int, help="lower index for spectrum boundary")
 
-    parser.add_argument("-t","--top",default = None,type=int,help="higher index for spectrum boundary")
+    parser.add_argument("-t", "--top", default=None, type=int, help="higher index for spectrum boundary")
 
-    parser.add_argument("--threshold",default = 0.05,type=float,help="Threshold level used for defining getting peaks for calibration")
+    parser.add_argument("--threshold", default=0.05, type=float,
+                        help="Threshold level used for defining getting peaks for calibration")
 
-    parser.add_argument("-p","--plot",action='store_false',help="don't plot the spectrum")
+    parser.add_argument("-p", "--plot", action='store_false', help="don't plot the spectrum")
 
-    parser.add_argument("-r","--recalibrate",action='store_true',help="Force recalibration. Otherwise it will silently use a previous calibration if one exists")
+    parser.add_argument("-r", "--recalibrate", action='store_true',
+                        help="Force recalibration. Otherwise it will silently use a previous calibration if one exists")
 
-    parser.add_argument("--width",default = 1.5,type=float,help="Number of FWHMs to use for extracting spectrum")
+    parser.add_argument("--width", default=1.5, type=float, help="Number of FWHMs to use for extracting spectrum")
 
     # parser.add_argument("-s","--dont-save",action='store_true',help="Don't save the spectra?")
 
-    parser.add_argument("--batch",action='store_true',help="Don't open images")
+    parser.add_argument("--batch", action='store_true', help="Don't open images")
 
-    parser.add_argument("-o","--order",default=2,type=int,help="Don't open images")
+    parser.add_argument("-o", "--order", default=2, type=int, help="Don't open images")
 
-    parser.add_argument("--zap-cosmic-rays",default=False,help=""""Get rid of cosmic rays.
+    parser.add_argument("--zap-cosmic-rays", default=False, help=""""Get rid of cosmic rays.
                                                         This uses simple algorithm that could clip bright lines.
                                                         Best used only for long integrations of diffuse nebular emission""")
 
@@ -117,7 +114,7 @@ if __name__ == "__main__":
     bottom = args.bottom
     top = args.top
 
-    rectify = True #args.rectify
+    rectify = True  # args.rectify
 
     zap_cosmic_rays = args.zap_cosmic_rays
 
@@ -125,7 +122,7 @@ if __name__ == "__main__":
 
     order = args.order
 
-    save = True #not args.dont_save
+    save = True  # not args.dont_save
 
     slice_fwhm = args.width
 
@@ -137,18 +134,17 @@ if __name__ == "__main__":
     else:
         recal = 'n'
 
-    out = reduce(cal_file, data_file, cal_threshold=threshold, bottom = bottom, top = top, order = order, rectify=rectify, cosmic_rays=zap_cosmic_rays, save=save, slice_fwhm=slice_fwhm, recal=recal)
-
+    out = reduce(cal_file, data_file, cal_threshold=threshold, bottom=bottom, top=top, order=order,
+                 rectify=rectify, cosmic_rays=zap_cosmic_rays, save=save, slice_fwhm=slice_fwhm, recal=recal)
 
     if plot:
-        w,spec,noise = out
+        w, spec, noise = out
         plt.figure(facecolor='w')
-        plt.fill_between(w,spec-noise,spec+noise)
-        plt.plot(w,spec)
+        plt.fill_between(w, spec-noise, spec+noise)
+        plt.plot(w, spec)
         plt.xlabel('Wavelength [nm]')
         plt.ylabel('arbitraty data units')
         fname = f"{data_file.replace('.FIT','.png')}"
         plt.savefig(fname)
         if not args.batch:
             plt.show(block=True)
-
